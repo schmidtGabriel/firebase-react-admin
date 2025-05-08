730057edbedb29f3b68817f597b4f40c42516b88
schmidtGabriel <g.avilasouza@gmail.com>
2025-05-08 12:08:35 -0300

    change title

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -7,7 +7,7 @@ export default function ProtectedRoute({ children }: { children: ReactNode }) {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return <div className="text-center mt-10">Carregando...</div>;
+    return <div className="text-center mt-10">Loading...</div>;
   }
 
   if (!user) return <Navigate to="/login" replace />;
