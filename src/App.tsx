86a6ca0404db5e19a64b95b8530589406b3c8c86
schmidtGabriel <g.avilasouza@gmail.com>
2025-05-08 11:56:39 -0300

    add darkmode e sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // src/App.tsx
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
+import SidebarLayout from "./components/ui/Sidebar";
 import { AuthProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -12,20 +13,24 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
 
           <Route
-            path="/users"
+            path="/*"
             element={
               <ProtectedRoute>
-                <Users />
+                <SidebarLayout>
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route
+                      path="/users"
+                      element={
+                        <ProtectedRoute>
+                          <Users />
+                        </ProtectedRoute>
+                      }
+                    />
+                  </Routes>
+                </SidebarLayout>
               </ProtectedRoute>
             }
           />
